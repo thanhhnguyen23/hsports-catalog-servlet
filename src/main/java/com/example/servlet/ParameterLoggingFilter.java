c169ebb9af087a208840ba4337a25edc59b815a5
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2023-07-24 01:16:56 -0400

    add basic servlet filter for every request for URL paramters

---
+++ src/main/java/com/example/servlet/ParameterLoggingFilter.java
@@ -0,0 +1,51 @@
+package com.example.servlet;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+/**
+ * Servlet Filter implementation class ParameterLoggingFilter
+ */
+@WebFilter("/*")
+public class ParameterLoggingFilter extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public ParameterLoggingFilter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		request.getParameterMap().entrySet().stream().forEach(entry -> {
+			System.out.println(String.format("%s:%s", entry.getKey(), entry.getValue()[0]));
+		});
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
